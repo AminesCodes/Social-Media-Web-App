8798f18d53ef3aede3478a489f667c05d4a6b766
vonbarown <vonielbrown@pursuit.org>
2019-11-06 22:03:17 -0500

    export comment

--- Database/database.js
+++ Database/database.js
@@ -3,6 +3,7 @@ const pgp = require('pg-promise')(); // import promise
 const connectionString = "postgres://localhost:5432/facebook_db" //URL where Postgres is running
 const db = pgp(connectionString); //connected db instance
 
+//exporting the db instance for use in routers
 module.exports = {
     db
 }
