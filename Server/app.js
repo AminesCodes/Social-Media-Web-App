7f1d11072ca52f90ef31a432404174015b4690f5
AminesCodes <aminebensalem@pursuit.org>
2019-11-07 11:02:40 -0500

    Fixed a bug on the port number

--- Server/app.js
+++ Server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = 5432;
+const port = 3131;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
