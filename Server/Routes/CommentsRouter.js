081063b1df9649e9803a08642bbc1ada6db3bc5e
AminesCodes <aminebensalem@pursuit.org>
2019-11-07 11:44:27 -0500

    Initialized the routes be to ba able to run the app.js(server)

---
+++ Server/Routes/CommentsRouter.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+//pg-promise request
+const {db} = require('../../Database/database'); //connected db instance
+
+
+
+module.exports = router;
