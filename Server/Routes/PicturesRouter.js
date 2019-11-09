db6d861a47be783d82098dd4a916630d3fbd3cd9
sergiocohens <sergiocohensalama@pursuit.org>
2019-11-09 11:07:15 -0500

    addedauthentication

--- Server/Routes/PicturesRouter.js
+++ Server/Routes/PicturesRouter.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 //pg-promise request
-const {db} = require('../../Database/database'); //connected db instance
+const {db} = require('../../Database/database')
 
-//functions
+//DB Query functions
 const getPicturesByAlbum = async (req, res) => {
   try {
     const requestQuery = `
@@ -59,9 +59,68 @@ const deletePicture = async (req, res) => {
   }
 }
 
-//routes
+//Authentication
+const checkValidAuthenticationBody = (req, res, next) => {
+    const username = request.body.loggedUsername
+    const password = request.body.loggedPassword
+    if (!username || !password) {
+        res.status(400)
+        res.json({
+            status: 'failed',
+            message: 'Missing authentication information'
+        })
+    } else {
+        req.loggedUsername = username.toLowerCase()
+        req.loggedPassword = password
+        next()
+    }
+}
+
+const checkIfUsernameExists = async (request, response, next) => {
+    try {
+        const user = await db.one('SELECT * FROM users WHERE username = $1', [req.loggedUsername])
+        req.userExists = true
+        req.targetUser = user
+        next()
+    } catch(err) {
+        if (err.received === 0) {
+            req.userExists = false
+            next()
+        } else {
+            res.status(500)
+            console.log(err)
+            res.json({
+                status: 'failed',
+                message: 'Something went wrong!'
+            })
+        }
+    }
+}
+
+const authenticateUser = (req, res, next) => {
+  if (req.userExists) {
+      if (req.targetUser.username === req.loggedUsername
+       && req.targetUser.user_password === req.loggedPassword) {
+              next()
+      } else {
+          res.status(401)
+          res.json({
+              status: 'failed',
+              message: 'Not authorized to accomplish the request'
+          })
+      }
+  } else {
+      res.status(404)
+      res.json({
+          status: 'failed',
+          message: 'User Does not exist'
+      })
+  }
+}
+
+//Routes
 router.get('/:albumid', getPicturesByAlbum)
-router.post('/:albumid', postPicture)
-router.delete('/:pictureid', deletePicture)
+router.post('/:albumid', checkValidAuthenticationBody, checkIfUsernameExists, authenticateUser, postPicture)
+router.delete('/:pictureid', checkValidAuthenticationBody, checkIfUsernameExists, authenticateUser, deletePicture)
 
 module.exports = router;
