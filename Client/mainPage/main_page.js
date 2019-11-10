0d6261e55ca81dc1f615be728addff790f520b6d
vonbarown <vonielbrown@pursuit.org>
2019-11-10 12:02:22 -0500

    working on toggle button

--- Client/mainPage/main_page.js
+++ Client/mainPage/main_page.js
@@ -1,11 +1,22 @@
 let url;
 
 document.addEventListener('DOMContentLoaded', () => {
-    loadTimesLikedData()
+    loadPostsTimesLikedData();
+    loadPictureTimesLikedData()
+
+    let feedForm = document.querySelector('#feedForm');
+    let postDiv = document.querySelector('#postsContainer')
+    let picDiv = document.querySelector('#picturesContainer').style.display = 'none'
+
+    feedForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        postDiv.style.display = 'none';
+        picDiv.style.display = 'true'
+    })
 })
 
 // this function loads the trending(times a post is liked) likes from the database
-const loadTimesLikedData = async () => {
+const loadPostsTimesLikedData = async () => {
     url = `http://localhost:3131/likes/posts/times_liked`
     const {
         data
@@ -14,39 +25,80 @@ const loadTimesLikedData = async () => {
     console.log(data);
 
     data.body.forEach(el => {
-        creatingCard(el)
+        creatingCardPost(el)
     });
 
     return data
 }
 
+// this function loads the trending(times a post is liked) likes from the database
+const loadPictureTimesLikedData = async () => {
+    url = `http://localhost:3131/likes/pictures/times_liked`
+    const {
+        data
+    } = await axios.get(url);
+
+    console.log(data);
+
+    data.body.forEach(el => {
+        creatingCardPic(el)
+    });
+
+    return data
+}
+
+const clearScreen = async () => {
+    container = getContainer()
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
+}
 
 
-// retrieving the container to display the feed
-const getContainer = () => document.querySelector('#container')
+// retrieving the feed sub-containers
+const getPostsContainer = () => document.querySelector('#postsContainer')
+const getPicturesContainer = () => document.querySelector('#picturesContainer')
 
 //This function create the cards on the create that will hold the axios information
-const creatingCard = async (el) => {
-    container = getContainer()
+const creatingCardPost = async (el) => {
+    const postsContainer = getPostsContainer()
 
     //creating the elements that will hold the information on the pokemon
-    const subContainer = creatingElem('div')
-    subContainer.className = 'likes'
-    const pic = creatingElem('img')
-    let posterUsername = document.createElement('p')
-    const body = document.createElement('p')
-    const times_liked = document.createElement('p')
+    const postSubContainer = creatingElem('div');
+    postSubContainer.className = 'likes';
+    let posterUsername = creatingElem('p');
+    let body = creatingElem('p');
+    let times_liked = creatingElem('p');
 
-    posterUsername = `The post by: ${el.poster_username}`
+    //assigning the innerText fore the posts
+    posterUsername.innerText = `This post by: ${el.poster_username}`
     body.innerText = `Text: ${el.body}`
-    times_liked.innerText = `Liked: ${el.times_liked} times`
+    times_liked.innerText = `Liked: ${el.times_liked} times`;
 
-    //Check if the data returned is a post or not (by checking of post id exits)
     //then appends the newly created elements to the subContainer  
-    el.post_id ? subContainer.append(posterUsername, body, times_liked) : subContainer.append(times_liked)
+    postSubContainer.append(posterUsername, body, times_liked);
+
     //appending thd subContainer that holds the created elements to the container
-    container.appendChild(subContainer)
+    postsContainer.append(postSubContainer);
 }
+//creating cards for pictures
+const creatingCardPic = async (el) => {
+    const picturesContainer = getPicturesContainer()
+
+    const picSubContainer = creatingElem('div');
+    picSubContainer.className = 'likes';
+    
+    let pic = creatingElem('img');
+    
+    //assigning the innerText for the pictures
+    pic.src = el.picture_link;
+
+    //then appends the newly created elements to the subContainer  
+    picSubContainer.append(pic)
+    //appending thd subContainer that holds the created elements to the container
+    picturesContainer.append(picSubContainer)
+}
+
 //this function creates elements
 function creatingElem(elem) {
     return document.createElement(`${elem}`)
