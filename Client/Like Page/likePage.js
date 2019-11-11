3cd25fd53bac449d53550d80e0e8e73ac9216f07
vonbarown <vonielbrown@pursuit.org>
2019-11-10 21:48:24 -0500

    starting to wotk on the likes page

---
+++ Client/Like Page/likePage.js
@@ -0,0 +1,6 @@
+let loggedUsername = sessionStorage.getItem('loggedUsername');
+let loggedPassword = sessionStorage.getItem('loggedPassword');
+let targetUser = sessionStorage.getItem('targetUser');
+
+
+document.addEventListener('DOMContentLoaded', () => {});
